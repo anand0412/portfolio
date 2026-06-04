d814ab641d3728afea1a407d94561b45d64b1108
anand0412 <xsslgr1007@gmail.com>
2026-06-04 10:12:15 +0530

    added java script

--- main.js
+++ main.js
@@ -1,1 +1,43 @@
-console.log('Anand Portfolio Phase 1 Loaded');
+console.log('Anand Portfolio Phase 1 Loaded');const buttons = document.querySelectorAll(".filter-btn");
+const cards = document.querySelectorAll(".gallery .card");
+
+buttons.forEach(button => {
+
+    button.addEventListener("click", () => {
+
+        buttons.forEach(btn =>
+            btn.classList.remove("active")
+        );
+
+        button.classList.add("active");
+
+        const filter =
+            button.getAttribute("data-filter");
+
+        cards.forEach(card => {
+
+            if(filter === "all"){
+
+                card.style.display = "block";
+
+            }
+
+            else if(
+                card.classList.contains(filter)
+            ){
+
+                card.style.display = "block";
+
+            }
+
+            else{
+
+                card.style.display = "none";
+
+            }
+
+        });
+
+    });
+
+});
